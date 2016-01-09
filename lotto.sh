89fb4f2deb02584d8dc62ba421f351f192d3f33f
William Strucke <william.strucke@klarna.com>
2016-01-08 23:38:05 -0500

    bug fix

--- lotto.sh
+++ lotto.sh
@@ -49,14 +49,15 @@ return 0
 function colorize_output {
   local string="$1"; shift
   local c=0
-  grep -qE '(^| )('$1'|'$2'|'$3'|'$4'|'$5')( |$)'
+  echo "$string" |grep -qE '(^| )('$1'|'$2'|'$3'|'$4'|'$5')( |$)'
   while [ $? -eq 0 ]; do
-    string=$( echo -e "$( echo $string |sed -E 's/ ([0-9]{5})/ '$B'[\1]'$STOP'/; s/(^| )('$1'|'$2'|'$3'|'$4'|'$5')( |$)/\1'$G'\2'$STOP'\3/' )" )
+    string=$( echo -e "$( echo $string |sed -E 's/(^| )('$1'|'$2'|'$3'|'$4'|'$5')( |$)/\1'$G'\2'$STOP'\3/' )" )
     c=$((c+1))
-    if [[ $c -gt 10 ]]; then exit 1; fi
+    if [[ $c -gt 10 ]]; then echo "error"; exit 1; fi
     echo "$string" |grep -qE '(^| )('$1'|'$2'|'$3'|'$4'|'$5')( |$)'
   done
-  echo -e "$( echo $string |sed -E 's/\+'$6' /\+'${G}${6}${STOP}' /' )"
+  echo -e "$( echo $string |sed -E 's/ ([0-9]{5})/ '$B'[\1]'$STOP'/; s/\+'$6' /\+'${G}${6}${STOP}' /' )"
+  return 0
 }
 
 function usage {
@@ -91,18 +92,24 @@ if [ ! -f numbers.txt ]; then usage; fi
 
 DEBUG=0
 W=0
+N1="$1"
+N2="$2"
+N3="$3"
+N4="$4"
+N5="$5"
+N6="$6"
 
 while read line; do
   BONUS=0
   MATCH=0
-  echo $line |grep -qE '^'$1' ' && MATCH=$(($MATCH+1))
-  echo $line |grep -qE " $2 " && MATCH=$(($MATCH+1))
-  echo $line |grep -qE " $3 " && MATCH=$(($MATCH+1))
-  echo $line |grep -qE " $4 " && MATCH=$(($MATCH+1))
-  echo $line |grep -qE " $5 " && MATCH=$(($MATCH+1))
-  echo $line |grep -qE '\+'$6'' && BONUS=1
+  echo $line |grep -qE '^'$N1' ' && MATCH=$(($MATCH+1))
+  echo $line |grep -qE " $N2 " && MATCH=$(($MATCH+1))
+  echo $line |grep -qE " $N3 " && MATCH=$(($MATCH+1))
+  echo $line |grep -qE " $N4 " && MATCH=$(($MATCH+1))
+  echo $line |grep -qE " $N5 " && MATCH=$(($MATCH+1))
+  echo $line |grep -qE '\+'$N6'' && BONUS=1
   check_win $MATCH $BONUS
-  test $? -eq 0 && colorize_output "$line" $@
+  test $? -eq 0 && colorize_output "$line" $N1 $N2 $N3 $N4 $N5 $N6
 done <numbers.txt
 
 if [[ "$W" -eq 0 ]]; then
